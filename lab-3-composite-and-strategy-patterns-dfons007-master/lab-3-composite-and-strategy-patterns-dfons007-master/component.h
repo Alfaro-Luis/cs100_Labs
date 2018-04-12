61f7074e2199ad61e4a40cf821923b65e937e72b
dfons007 <dfons007@ucr.edu>
2018-04-12 15:12:57 -0700

    Revert "All Classes done. Test Case Done in Test.cpp."
    This reverts commit 1ed0f47d1c92adc5e3312e3c446385dfc555a6bc.

--- lab-3-composite-and-strategy-patterns-dfons007-master/lab-3-composite-and-strategy-patterns-dfons007-master/component.h
+++ lab-3-composite-and-strategy-patterns-dfons007-master/lab-3-composite-and-strategy-patterns-dfons007-master/component.h
@@ -3,127 +3,128 @@
 
 #include <stddef.h>
 #include <cmath>
-
-
-//This is our base class
-class base {
-public:
-	double data;
-	base *right;
-	base *left;
-
-	/* constructors */
-	base() {
-		this->data = 0;
-		this->right = NULL;
-		this->left = NULL;
-	}
-
-	base(base *rightone, base *leftone)
-	{
-		this->right = rightone;
-		this->left = leftone;
-		this->data = evaluate();
-	}
-
-	/*My Functions*/
-
-	/* pure virtual functions */
-	virtual double evaluate() = 0;
-};
+#include <string>
+#include <sstream>
 // This will be our opperand
-class Op :public base
+class Op
 {
 public:
-	//Constructor
-	Op() {
-		this->data = 0;
+	std::string data;
+	Op *left;
+	Op *right;
+
+	Op(char data){
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
 	}
 
-	Op(double x)
+	std::string checkdata()
 	{
-		this->data = x;
-	}
-	//Functions
-	double evaluate() {
-		return this->data;
-	}
-};
-
-class Add :public base
-{
-public:
-	Add(base *leftop, base *rightop) {
-		this->right = rightop;
-		this->left = leftop;
-		this->data = this->left->evaluate() + this->right->evaluate();
-
-	}
-	double evaluate() {
-		this->data = this->left->evaluate() + this->right->evaluate();
 		return this->data;
 	}
 
-};
-
-class Sub :public base
-{
-public:
-	Sub(base *leftop, base *rightop) {
-		this->right = rightop;
-		this->left = leftop;
-		this->data = this->left->evaluate() - this->right->evaluate();
-
-	}
-	double evaluate() {
-		this->data = this->left->evaluate() - this->right->evaluate();
-		return this->data;
-	}
 	
 };
 
-class Mult :public base
+class Mult :public Op
 {
 public:
-	Mult(base *leftop, base *rightop) {
-		this->right = rightop;
-		this->left = leftop;
-		this->data = this->left->evaluate() * this->right->evaluate();
-
-	}
-	double evaluate() {
-		this->data = this->left->evaluate() * this->right->evaluate();
-		return this->data;
-	}
-};
-
-class Div :public base
-{
-public:
-	Div(base *leftop, base *rightop) {
-		this->right = rightop;
-		this->left = leftop;
-		this->data = this->left->evaluate() / this->right->evaluate();
-
-	}
-	double evaluate() {
-		this->data = this->left->evaluate() / this->right->evaluate();
-		return this->data;
-	}
-};
-
-class Sqr :public base
-{
-public:
-	Sqr(base *operand)
+	int mult()
 	{
-		this->right = operand;
-		this->data = sqrt(right->evaluate());
+		std::string l, r;
+		l = this->left->checkdata();
+		r = this->right->checkdata();
+		int leftop = std::stoi(l);
+		int rightop = std::stoi(r);
+		return leftop * rightop;
 	}
-	double evaluate() {
-		this->data = sqrt(right->evaluate());
-		return this->data;
+};
+
+class Div :public Op
+{
+public:
+	double div()
+	{
+		std::string l, r;
+		l = this->left->checkdata();
+		r = this->right->checkdata();
+		int leftop = std::stoi(l);
+		int rightop = std::stoi(r);
+		return leftop / rightop;
 	}
+
+};
+
+class Add :public Op
+{
+public:
+	double add()
+	{
+		std::string l, r;
+		l = this->left->checkdata();
+		r = this->right->checkdata();
+		int leftop = std::stoi(l);
+		int rightop = std::stoi(r);
+		return leftop + rightop;
+	}
+};
+
+class Sub :public Op
+{
+public:
+	double sub()
+	{
+		std::string l, r;
+		l = this->left->checkdata();
+		r = this->right->checkdata();
+		int leftop = std::stoi(l);
+		int rightop = std::stoi(r);
+		return leftop - rightop;
+	}
+
+};
+
+class Sqr :public Op
+{
+public:
+	double sqr() 
+	{
+		std::string l, r;
+		l = this->left->checkdata();
+		r = this->right->checkdata();
+		int leftop = std::stoi(l);
+		int rightop = std::stoi(r);
+		return leftop * rightop;
+	}
+};
+
+// Is this our Tree???
+class base {
+    public:
+
+        /* constructors */
+        base() {}
+
+		/*My Functions*/
+		bool amOp(char opCheck);
+		double performop();
+        /* pure virtual functions */
+        virtual double evaluate() = 0;
 };
 
 #endif // __component_h__
+
+bool base::amOp(char opCheck)
+{
+	if (opCheck == '*' || opCheck == '+' || opCheck == '-' || opCheck == '/' || opCheck == '^')
+	{
+		return true;
+	}
+	return false;
+}
+
+double base::performop()
+{
+
+}
