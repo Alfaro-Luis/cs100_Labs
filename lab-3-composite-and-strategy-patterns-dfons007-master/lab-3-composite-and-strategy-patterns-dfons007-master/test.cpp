b59189c4fca9b280ac0a18a81fff84bc9a6cf0d8
dfons007 <dfons007@ucr.edu>
2018-04-14 21:24:56 -0700

    Test File. Both Sorts working after testing. Try and catch block for sort works. Lab completely finished.

--- lab-3-composite-and-strategy-patterns-dfons007-master/lab-3-composite-and-strategy-patterns-dfons007-master/test.cpp
+++ lab-3-composite-and-strategy-patterns-dfons007-master/lab-3-composite-and-strategy-patterns-dfons007-master/test.cpp
@@ -1,35 +1,57 @@
 #include <iostream>
 #include "component.h"
-//#include "vector_container.h"
-//#include "list_container.h"
-//#include "strategy.h"
+#include "VectorContainer.h"
+#include "ListContainer.h"
+#include "strategy.h"
 
 using namespace std;
 
 int main() {
-    //Op* op1 = new Op(5);
-    //Op* op2 = new Op(2);
-    //Op* op3 = new Op(4);
-    //Op* op4 = new Op(6);
-    //Mult* A = new Mult(op1, op2);
-    //Sub* B = new Sub(op3, op4);
-    //Add* C = new Add(A, B);
-    //Sqr* D = new Sqr(C);
 
-    //VectorContainer* container = new VectorContainer();
-    ////ListContainer* container = new ListContainer();
-    //container->add_element(A);
-    //container->add_element(B);
-    //container->add_element(D);
-    //cout << "Container Before Sort: " << endl;
-    //container->print();
-    //container->set_sort_function(new SelectionSort());
-    //container->sort();	
-    //cout << "Container After Sort: " << endl;
-    //container->print();
 	Op* op5 = new Op(5);
 	Op* op4 = new Op(4);
+	Op* op12 = new Op(12);
+	Op* op13 = new Op(13);
 	Add* C = new Add(op5, op4);
 	Mult* D = new Mult(C, op4);
-	cout << D->evaluate() << endl;
+
+
+
+	VectorContainer* container = new VectorContainer(D);
+	ListContainer* listcontainer = new ListContainer(D);
+
+	container->set_sort_function(new SelectionSort());
+	container->add_element(C);
+	container->add_element(op5);
+	container->add_element(op4);
+	container->add_element(op12);
+	container->add_element(op13);
+
+	cout << "Vector Container Selection sort" << "\n" << "\n";
+
+	cout << "Before Sort" << "\n" << "\n";
+	container->print();
+	container->sort();
+	cout << "After Sort" << "\n" << "\n";
+	container->print();
+
+	cout << "\n" << "List Container Bubble sort" << "\n" << "\n";
+
+	
+	listcontainer->set_sort_function(new BubbleSort());
+	listcontainer->add_element(C);
+	listcontainer->add_element(op5);
+	listcontainer->add_element(op4);
+	listcontainer->add_element(op12);
+	listcontainer->add_element(op13);
+
+	cout << "Before Sort" << "\n" << "\n";
+	listcontainer->print();
+	listcontainer->sort();
+	cout << "\n" << "After Sort" << "\n" << "\n";
+	listcontainer->print();
+
+
+	
+
 };
