fb42d70ef06741061129876de95a73592cf70f2c
dfons007 <dfons007@ucr.edu>
2018-04-14 20:02:02 -0700

    Selection Sort. Done.

--- lab-3-composite-and-strategy-patterns-dfons007-master/lab-3-composite-and-strategy-patterns-dfons007-master/strategy.h
+++ lab-3-composite-and-strategy-patterns-dfons007-master/lab-3-composite-and-strategy-patterns-dfons007-master/strategy.h
@@ -1,6 +1,9 @@
 #ifndef __STRATEGY_H__
 #define __STRATEGY_H__
 
+#include "container.h"
+#include "component.h"
+
 class Sort {
     public: 
         /* Constructors */
@@ -9,5 +12,47 @@ class Sort {
         /* Pure Virtual Functions */
         virtual void sort(Container* container) = 0;
 };
+
+Sort::Sort()
+{
+
+}
+
+
+class SelectionSort :public Sort
+{
+public:
+	SelectionSort();
+	void sort(Container* container);
+
+};
+
+SelectionSort::SelectionSort()
+{
+}
+
+void SelectionSort::sort(Container* container)
+{
+	for (int i = 0; i < container->size() - 1; i++)
+	{
+		int minindex = i;
+		for (int j = i + 1; j < container->size(); j++)
+		{
+			if (container->at(j)->evaluate() < container->at(minindex)->evaluate())
+				minindex = j;
+		}
+		std::cout << "Swapping" << container->at(i)->evaluate() << " and " << container->at(minindex)->evaluate() << std::endl;
+		container->swap(i,minindex);
+	}
+
+}
+
+
+class BubbleSort :public Sort
+{
+public:
+
+};
+
 
 #endif // __STRATEGY_H__
